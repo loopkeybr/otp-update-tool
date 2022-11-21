9585c8a0ae9074f432f431b2bb48572c3957d76c
gugagop <gugagop4@gmail.com>
2022-11-21 10:55:39 -0300

    Create cert directory if not created

--- exec.sh
+++ exec.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # create certificate files
-echo $MQTT_CERTIFICATE_B64 | base64 --decode > cert/cert-certificate.pem.crt
-echo $MQTT_PRIVATE_KEY_B64 | base64 --decode > cert/cert-private.pem.key
-echo $MQTT_PUBLIC_KEY_B64 | base64 --decode > cert/cert-public.pem.key
-echo $MQTT_ROOT_CA_B64 | base64 --decode > cert/root-CA.crt
+
+mkdir -p cert
+echo $MQTT_CERTIFICATE_B64 | base64 --decode > /cert/cert-certificate.pem.crt
+echo $MQTT_PRIVATE_KEY_B64 | base64 --decode > /cert/cert-private.pem.key
+echo $MQTT_PUBLIC_KEY_B64 | base64 --decode > /cert/cert-public.pem.key
+echo $MQTT_ROOT_CA_B64 | base64 --decode > /cert/root-CA.crt
 
 
 python3 app.py
