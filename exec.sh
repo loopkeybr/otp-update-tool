e2c62b8e89c7f2e4825ddfe98e1aa8bc201d0138
gugagop <gugagop4@gmail.com>
2022-11-21 14:18:51 -0300

    Create app directory if not created

--- exec.sh
+++ exec.sh
@@ -3,10 +3,11 @@
 # create certificate files
 
 mkdir -p cert
+mkdir -p app
 echo $MQTT_CERTIFICATE_B64 | base64 --decode > /cert/cert-certificate.pem.crt
 echo $MQTT_PRIVATE_KEY_B64 | base64 --decode > /cert/cert-private.pem.key
 echo $MQTT_PUBLIC_KEY_B64 | base64 --decode > /cert/cert-public.pem.key
 echo $MQTT_ROOT_CA_B64 | base64 --decode > /cert/root-CA.crt
 
 python3 app.py
-# scripts/version_get_gw LLKGAADAAAB6L6DS
+exit
