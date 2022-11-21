f0280637056eaed36ee3e4e02f55a97c8679d4a7
gugagop <gugagop4@gmail.com>
2022-11-21 11:17:27 -0300

    Change something to create new package

--- exec.sh
+++ exec.sh
@@ -8,6 +8,5 @@ echo $MQTT_PRIVATE_KEY_B64 | base64 --decode > /cert/cert-private.pem.key
 echo $MQTT_PUBLIC_KEY_B64 | base64 --decode > /cert/cert-public.pem.key
 echo $MQTT_ROOT_CA_B64 | base64 --decode > /cert/root-CA.crt
 
-
 python3 app.py
 # scripts/version_get_gw LLKGAADAAAB6L6DS
